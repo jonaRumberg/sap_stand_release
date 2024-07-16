095b8e8d9e7674198280405636d683ba7e1d3e03
Jona Rumberg <jona.rumberg@sap.com>
2024-07-16 10:49:12 +0200

    fix routing

--- stand-frontend/src/routes/index.tsx
+++ stand-frontend/src/routes/index.tsx
@@ -1,14 +1,17 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/ErrorPage";
 import Launchpad from "../pages/FLP";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
         element: <App />,
         errorElement: <ErrorPage />,
-        children: [{ path: "/", element: <Launchpad /> },
-                   { path: "/results", element: <Launchpad /> },
-                   { path: "*", element: <Launchpad /> }]
+
+        children: [
+            { path: "/", element: <Launchpad /> },
+            { path: "/results", element: <Launchpad /> },
+            { path: "*", element: <Launchpad /> }
+        ]
     }
 ]);
