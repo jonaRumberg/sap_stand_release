95f6270ae0c2c02785e6dfbcbe51ed6293a822a7
I569996 <maximilian.otto@sap.com>
2024-07-17 08:53:25 +0200

    removed shellbar

--- stand-frontend/src/pages/CreateProductionOrder.tsx
+++ stand-frontend/src/pages/CreateProductionOrder.tsx
@@ -69,33 +69,6 @@ import {
   
     return (
       <>
-        {/* <ShellBar
-          logo={<img alt="SAP Logo" src="https://sap.github.io/ui5-webcomponents/images/sap-logo-svg.svg" />}
-          menuItems={<><CustomListItem data-key="1">Menu Item 1</CustomListItem><CustomListItem data-key="2">Menu Item 2</CustomListItem><CustomListItem data-key="3">Menu Item 3</CustomListItem></>}
-          notificationsCount="10"
-          // onCoPilotClick={function _a() { } }
-          // onLogoClick={function _a() { } }
-          // onMenuItemClick={function _a() { } }
-          // onNotificationsClick={function _a() { } }
-          // onProductSwitchClick={function _a() { } }
-          // onProfileClick={function _a() { } }
-          // onSearchButtonClick={function _a() { } }
-          primaryTitle="Produktion"
-          profile={<Avatar><img src="https://sap.github.io/ui5-webcomponents-react/assets/Person-B7wHqdJw.png" /></Avatar>}
-          searchField={<Input icon={<Icon interactive name="search" />} showClearIcon />}
-          secondaryTitle="Fiori 3 Shell Bar"
-          showCoPilot
-          showNotifications
-          showProductSwitch
-        >
-        <ShellBarItem
-          count="3"
-          icon="add"
-          text="ShellBarItem" />
-        </ShellBar> */}
-        {/* <div style={{
-          height: "calc(100vh - 304px)"
-        }}> */}
   
         <Page
           backgroundDesign="Solid"
