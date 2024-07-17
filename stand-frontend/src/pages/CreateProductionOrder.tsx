310de02bbc92073f9a248d6d98fdf99296c2d741
I568870 <isabel.heuser@sap.com>
2024-07-17 16:53:21 +0200

    update layout in confirmation box

--- stand-frontend/src/pages/CreateProductionOrder.tsx
+++ stand-frontend/src/pages/CreateProductionOrder.tsx
@@ -68,6 +68,10 @@ const CreateProductionOrder = () => {
       setDiasbled(false);
     }
   }
+  const error = useRef(null);
+  const errorToast = () => {
+    error.current.show();
+  }
 
   const productionLineChange = (event) => {
 
@@ -124,23 +128,25 @@ const CreateProductionOrder = () => {
     <>
       <ObjectPage
         footer={
-           <Bar design="FloatingFooter"
-           endContent={<>
-              <Button ref={production} 
-              disabled={disabled} 
-              design="Emphasized" 
-              onClick={onButtonClick}>
-              Jetzt produzieren
-              </Button> 
-              <Button design="Transparent">
+          <Bar design="FloatingFooter"
+            endContent={<>
+              <Button ref={production}
+                disabled={disabled}
+                design="Emphasized"
+                onClick={onButtonClick}>
+                Jetzt produzieren
+              </Button>
+              <Button onClick={errorToast} design="Transparent">
                 Später einplanen
               </Button>
-              <Button design="Transparent">
+              <Button onClick={errorToast} design="Transparent">
                 Cancel
               </Button></>}>
-              </Bar>}
+          </Bar>}
         headerContent={<DynamicPageHeader></DynamicPageHeader>}
-        headerTitle={<DynamicPageTitle header="Produktionsauftrag anlegen"></DynamicPageTitle>}
+        headerTitle={
+          <DynamicPageTitle header="Produktionsauftrag anlegen"></DynamicPageTitle>
+        }
         imageShapeCircle
         selectedSectionId="Auftragsdetails"
         style={{
@@ -190,9 +196,6 @@ const CreateProductionOrder = () => {
           <FormGroup >
             <FormItem label="Werk ID">
               <Input type="Number" readonly value="1010" />
-            </FormItem>
-            <FormItem label="Linie">
-              <Input type="Number" readonly value="2" />
             </FormItem>
             <FormItem label="Name">
               <Input type="Text" readonly value="SAP Walldorf" />
@@ -248,33 +251,40 @@ const CreateProductionOrder = () => {
       </ObjectPage>
       <MessageBox
         open={open}
-
         onClose={handleClose}
         type="Confirm"
+        style={{ minWidth: '600px' }}
       >
-        Produktionszeitpunkt:
-        <DateTimePicker
-          readonly
-          onChange={function _a() { }}
-          onInput={function _a() { }}
-          onValueStateChange={function _a() { }}
-          primaryCalendarType="Gregorian"
-          valueState="None"
-          value="today"
-        />
-        Produktionslinie:
-        <ComboBox
-          icon={<Icon name="production" />}
-          onSelectionChange={productionLineChange}
-          valueState="None"
-          value="Linie 1"
-        >
-          <ComboBoxItem text="Linie 1" />
-          <ComboBoxItem text="Linie 2" />
-          <ComboBoxItem text="Linie 3" />
-        </ComboBox>
+        <Form style={{ minWidth: '500px' }}
+          columnsL={1}
+          columnsM={1}
+          columnsS={1}
+          columnsXL={1}>
+          <FormGroup titleText="Produktion" >
+            <FormItem label="Produktionszeitpunkt:">
+              <DateTimePicker
+                readonly
+                onChange={function _a() { }}
+                onInput={function _a() { }}
+                onValueStateChange={function _a() { }}
+                primaryCalendarType="Gregorian"
+                valueState="None"
+                value="today"
+              />
+            </FormItem>
+            <FormItem label="Produktionslinie:">
+              <Select>
+                <Option>Linie 1</Option>
+                <Option>Linie 2</Option>
+                <Option>Linie 3</Option>
+              </Select>
+            </FormItem>
+          </FormGroup>
+        </Form>
+
       </MessageBox>
       <Toast ref={toast}>Tipp: Wähle Gummibärchen aus!</Toast>
+      <Toast ref={error}>Nicht möglich</Toast>
     </>
 
   )
