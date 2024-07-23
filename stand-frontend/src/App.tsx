b62c838b243abf902b0f904abe982200bd02ac8e
Jona Rumberg <jona.rumberg@sap.com>
2024-07-23 19:15:21 +0200

    some minor fixes

--- stand-frontend/src/App.tsx
+++ stand-frontend/src/App.tsx
@@ -1,32 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { setTheme, isTheme } from "@ui5/webcomponents-base/dist/config/Theme";
-import { ShellBar, ShellBarItem} from "@ui5/webcomponents-react";
 import { Outlet } from "react-router-dom";
-import { useEffect } from "react";
-import {useCookies} from "react-cookie";
 
 function App() {
-    const [cookies, setCookie] = useCookies(['prefersDarkMode']);
-
-
-    const handleDarkModeSwitch = () => {
-        if (isTheme("sap_horizon")) {
-            setCookie("prefersDarkMode", true, {maxAge: 31536000});
-        } else {
-            setCookie("prefersDarkMode", false, {maxAge: 31536000});
-        }
-    }
-
-    useEffect(() => {
-        if (cookies?.prefersDarkMode == true) {
-            setTheme("sap_horizon_dark")
-        } else {
-            setTheme("sap_horizon")
-        }
-    }, [cookies?.prefersDarkMode]);
-
-
     return (
         <>
             <Outlet />
